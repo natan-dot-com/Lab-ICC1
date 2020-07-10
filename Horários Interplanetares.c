6b170d9651ae3626c8398ddc6805befcdc7cef6d
Natan Sanches <61665410+natan-dot-com@users.noreply.github.com>
2020-07-10 20:58:09 -0300

    Update Horários Interplanetares.c

--- Horários Interplanetares.c
+++ Horários Interplanetares.c
@@ -15,15 +15,15 @@ void printResult(unsigned long long *planet_time, unsigned long long sec, char *
 
 // Conversor para horário terrestre.
 void timeInEarth(unsigned long long **planet_time, unsigned long long sec) {
-    if (sec >= 86400) {								// Um dia na Terra equivale a 86400 segundos
+    if (sec >= 86400) {					// Um dia na Terra equivale a 86400 segundos
         (*planet_time)[0] = sec/86400;
         sec %= 86400;
     }
-    if (sec >= 3600) {								// Uma hora equivale a 3600 segundos  
+    if (sec >= 3600) {					// Uma hora equivale a 3600 segundos  
         (*planet_time)[1] = sec/3600;
         sec %= 3600;
     }
-    if (sec >= 60) {								// Um minuto equivale a 60 segundos					
+    if (sec >= 60) {					// Um minuto equivale a 60 segundos					
         (*planet_time)[2] = sec/60;
         sec %= 60; 
     }
@@ -32,7 +32,7 @@ void timeInEarth(unsigned long long **planet_time, unsigned long long sec) {
 
 // Conversor para horário em Vênus.
 void timeInVenus(unsigned long long **planet_time, unsigned long long sec) {
-	int DayInVenus = 243*24;						// Um dia em Vênus possui 243*24 *horas* terrestres 
+	int DayInVenus = 243*24;			// Um dia em Vênus possui 243*24 *horas* terrestres 
 	(*planet_time)[0] = (sec/3600) / DayInVenus;	// A divisão do número de horas em Vênus pela duração do dia nos dá o número de dias em Vênus
 	(*planet_time)[1] = (sec/3600) % DayInVenus;	// O resto da divisão acima nos dá o número de horas em Vênus desconsiderando os dias
 	(*planet_time)[2] = (sec/60) % 60;  
@@ -41,8 +41,8 @@ void timeInVenus(unsigned long long **planet_time, unsigned long long sec) {
 
 // Conversor para horário em Mercúrio.
 void timeInMercury(unsigned long long **planet_time, unsigned long long sec) {
-	int DayInMercury = 86400*58 + 3600*16;			 // Um dia em Mercúrio possui 86400*58 + 3600*16 *segundos* terrestres
-	(*planet_time)[0] = sec/DayInMercury;			 // A divisão do número de segundos em Mercúrio pela duração do dia nos dá o número de dias			
+	int DayInMercury = 86400*58 + 3600*16;		 // Um dia em Mercúrio possui 86400*58 + 3600*16 *segundos* terrestres
+	(*planet_time)[0] = sec/DayInMercury;		 // A divisão do número de segundos em Mercúrio pela duração do dia nos dá o número de dias			
 	(*planet_time)[1] = (sec%DayInMercury) / 3600;	 // O resto da divisão acima, convertida em horas, nos dá o número de horas em Mercúrio
 	(*planet_time)[2] = (sec%DayInMercury/60) % 60;
 	(*planet_time)[3] = sec % 60;
@@ -50,9 +50,9 @@ void timeInMercury(unsigned long long **planet_time, unsigned long long sec) {
 
 // Conversor para horário em Júpiter
 void timeInJupiter(unsigned long long **planet_time, unsigned long long sec) {
-	int DayInJupiter = 3600*9 + 60*56;				  // Um dia em Júpiter possui 3600*9 + 60*56 *segundos* terrestres 
-	(*planet_time)[0] = sec/DayInJupiter;			  // A divisão do número de segundos em Júpiter pela duração do dia nos dá o número de dias 
-	(*planet_time)[1] = (sec%DayInJupiter) / 3600;	  // O resto da divisão acima, depois de convertida em horas, devolve o número de horas no planeta
+	int DayInJupiter = 3600*9 + 60*56;		 // Um dia em Júpiter possui 3600*9 + 60*56 *segundos* terrestres 
+	(*planet_time)[0] = sec/DayInJupiter;		 // A divisão do número de segundos em Júpiter pela duração do dia nos dá o número de dias 
+	(*planet_time)[1] = (sec%DayInJupiter) / 3600;	 // O resto da divisão acima, depois de convertida em horas, devolve o número de horas no planeta
 	(*planet_time)[2] = (sec%DayInJupiter/60) % 60;
 	(*planet_time)[3] = sec % 60;
 }
